7835a8264dfd32a74a0f67f087ce8458c6d86ba4
haorenxwx <haorenxwx@users.noreply.github.com>
2017-09-07 19:10:47 +0800

    update
    update

--- python_notes3.py
+++ python_notes3.py
@@ -40,5 +40,46 @@ L2=list(map(normalize,L1))
 print(L2)
 
 #3，filter()
-#输入函数和字符串，
+#输入函数和字符串，函数依次作用于字符串的每个元素，根据返回值true/false决定保留或者丢弃
+def is_odd(n):
+	return n%2 == 1
+print(list(filter(is_odd,[1,2,3,4,5,6,7])))
+#删除序列中的空字符串
+#strip的用法
+s=['Facebook;Google+;MySpace', 'Apple;Android']
+s1=[ele.strip(' ').split(';') for ele in s]
+print(s1)
+#s=['A', '', 'B', None, 'C', '  ']
+#s2=[ele.strip() for ele in s]
+#print(s1)
+def un_blank(s):
+	return s and s.strip()
+print(list(filter(un_blank,['A', '', 'B', None, 'C', '  '])))
+#用filter求素数
+def _init():
+	n=1
+	while True:
+		n=n+2
+		yield n
+def not_dividible(n):
+	return lambda x :x%n>0
+def prime():
+	n=2
+	it=_init()
+	while True:
+		n=next(it)
+		filter(not_dividible(n),it)
+for n in prime():
+	if n <100:
+		print n
+	else:
+		break
+
+
+
+
+
+
+
+
 

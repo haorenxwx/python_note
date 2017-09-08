f52751e2865754a6f289a21edea706f4212e548e
haorenxwx <haorenxwx@users.noreply.github.com>
2017-09-08 15:21:21 +0800

    20170908again
    20170908again

--- python_notes3.py
+++ python_notes3.py
@@ -126,6 +126,14 @@ print(sorted(['gakki','umr','toda','emma'],key=str.lower))
 #按照字符串长度反向排序
 print(sorted(['gakki','umr','toda','emma'],key=str.lower,reverse=True))
 
+L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+def by_name(t):
+	return t[0]
+print(sorted(L,key=by_name))
+def by_score(t):
+	return t[1]
+print(sorted(L,key=by_score))
+
 
 
 
